ce5af43ded04f952c0fef24b347ef3a99adb070e
Alona Pakhniuk <a.pakhniuk@gmail.com>
2026-06-03 22:02:00 -0700

    Initial Commit

---
+++ BestShot/BestShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestShotApp.swift
+//  BestShot
+//
+//  Created by Alona Pakhniuk on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct BestShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestShotTests/BestShotTests.swift
@@ -0,0 +1,17 @@
+//
+//  BestShotTests.swift
+//  BestShotTests
+//
+//  Created by Alona Pakhniuk on 6/3/26.
+//
+
+import Testing
+@testable import BestShot
+
+struct BestShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
